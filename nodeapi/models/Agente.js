60234963faf1d55841039735d29277f7dc7b5190
David Álvarez <david11988@gmail.com>
2017-03-28 22:42:00 +0200

    Añadimos Mongoose y CRUD API

---
+++ nodeapi/models/Agente.js
@@ -0,0 +1,29 @@
+/**
+ * Created by david on 28/03/17.
+ */
+'use strict';
+
+const mongoose = require('mongoose');
+
+// primero definimos un esquema
+
+const agenteSchema = mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+// Creamos un método estático para recuperar agentes paginados
+agenteSchema.statics.list = function(filter, cb) {
+    const query = Agente.find(filter);
+    query.exec(cb);
+};
+
+// y luego creamos el modelo
+var Agente = mongoose.model('Agente', agenteSchema);
+
+// no necesitamos exportar el modelo ya que podriamos recuperarlo en cualquier momento con:
+// var Agente = mongoose.model('Agente');
+
+//const agente = new Agente({name: 'Creado desde mongoose', age: 31});
+//agente.save();
+
